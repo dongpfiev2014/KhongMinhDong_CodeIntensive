4fdaa47dabbe4f897f2d13fb3164fca945ab6339
dongpfiev2014 <dongpfiev2014@gmail.com>
2024-04-10 17:16:20 +0700

    Final Project

---
+++ smart-home-ecommerce/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Helmet } from "react-helmet";
+
+function App() {
+  return (
+    <>
+      <div>Dong vippro</div>
+      <Helmet>
+        <title>My Title</title>
+        <meta name="description" content="Helmet application" />
+      </Helmet>
+    </>
+  );
+}
+
+export default App;
